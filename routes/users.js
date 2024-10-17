1b016c9a7bce84796c3fb52119ea916a931d88d2
“sabiq” <sabiq.a@myclnq.co.in>
2024-10-17 21:03:06 +0530

    fix login response

--- routes/users.js
+++ routes/users.js
@@ -35,6 +35,16 @@ const loginSchema = Joi.object({
 
 })
 
+
+const grievienceSchema = Joi.object({
+  complaint: Joi.string().required(),
+  description: Joi.string().required(),
+  latitude: Joi.number().required(),
+  longitude: Joi.number().required(),
+  userId :Joi.string().required()
+
+})
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -63,6 +73,10 @@ router.post("/create", async function (req, res, next) {
     await client.db("streetSOS").command({ ping: 1 });
     const database = client.db("streetSOS");
     const users = database.collection("users");
+    if(findResult){
+      return res.status(400).json({ error:"user already exist" });
+    }
+    const findResult = await users.findOne({ email: email});
     const doc = {
       "name": name,
      "email":email,
@@ -97,21 +111,57 @@ const data = req.body;
     return res.status(400).json({ error:"user does'nt exist" });
   }
   if(findResult.email != email){
-    res.status(401).send('Invalid credentials');
+    res.status(401).send({error:'Invalid credentials'});
   }
   
   
   const passwordMatch = await bcrypt.compare(password, findResult.password);
 
     if (passwordMatch) {
-      res.send({message:'Login successful',userId: findResult._id});
+      res.status(200).send({message:'Login successful',userId: findResult._id});
     } else {
-      res.status(400).send('Invalid credentials');
+      res.status(400).send({error:'Invalid credentials'});
     }
 
  //res.status(200).send({'User found successfully':findResult});
 
 })
+
+router.post("/complaint", async function (req, res, next){
+  try{const data = req.body;
+  const { error } = grievienceSchema.validate(data);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  const { complaint, description, latitude, longitude,userId} = data
+  await client.connect();
+  const database = client.db("streetSOS");
+  const grievance = database.collection("grievance");
+  const doc = {
+    complaint: complaint,
+    description: description,
+    location: {
+      type: "Point",
+      coordinates: [longitude, latitude],
+    },
+    verified: false,
+    userID: ObjectId.createFromHexString(userId),
+  };
+  const result = await grievance.insertOne(doc);
+  res.status(201).send({'complaint registered successfully':result});
+}
+catch (error) {
+  console.error(error);
+  res.status(500).send('An error occurred');
+}
+finally{
+  await client.close();
+}
+
+})
+
+
+
 
 
 
